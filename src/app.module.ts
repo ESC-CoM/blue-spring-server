fba9915232f13bf45cf90ae9cfbbe5988af714b0
vcho1958 <gusckd57@gmail.com>
2023-01-12 11:16:25 +0900

    [feat] meeting module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MeetingModule } from './domain/meeting/meeting.module';
 
 @Module({
-  imports: [],
+  imports: [MeetingModule],
   controllers: [AppController],
   providers: [AppService],
 })
