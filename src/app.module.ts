a57e4b01f4201f0eb3f679cc7ce6ef944614d433
vcho1958 <gusckd57@gmail.com>
2023-01-22 01:55:29 +0900

    feat: assign user module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MeetingModule } from './domain/meeting/meeting.module';
+import { UserModule } from './domain/user/user.module';
 
 @Module({
-  imports: [MeetingModule],
+  imports: [MeetingModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
