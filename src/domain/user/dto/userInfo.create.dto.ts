a7d811bf813d54497939f81ec42672b66238c1f3
vcho1958 <gusckd57@gmail.com>
2023-01-22 01:51:35 +0900

    feat: user create test

---
+++ src/domain/user/dto/userInfo.create.dto.ts
@@ -0,0 +1,10 @@
+import { Gender, Role } from '../../../../prisma/user';
+
+export interface UserInfoCreateDto {
+  phone: string; //         String           @unique @db.Char(11)
+  nickname: string; //           @db.VarChar(12)
+  gender: Gender;
+  birthYear: number;
+  profileImageUrl: string;
+  role: Role;
+}
